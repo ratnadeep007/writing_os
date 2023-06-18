9f24078aaa45f6fc79415e8b0a942131476e592c
Ratnadeep Bhattacharyya <ratndeepofghato@gmail.com>
2023-06-18 18:31:54 +0530

    update kernel, bootloader and add FAT support

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,11 +1,17 @@
-org 0x7C00
+org 0x0
 bits 16
 
 %define ENDL 0x0D, 0x0A
 
 ; to jump to main
 start:
-  jmp main
+  ; print hello World
+  mov si, msg_hello
+  call puts
+
+.halt:
+  cli
+  hlt
 
 ; Prints a string to the screen
 ; Params:
@@ -14,47 +20,25 @@ puts:
   ; save registers we will modify
   push si
   push ax
+  push bx
 
 .loop:
   ; loads next character in al
   lodsb
-  ; verify is next character is null          
-  or al, al      
+  ; verify is next character is null
+  or al, al
   jz .done
 
   ; call bios interrupt
-  mov ah, 0x0e   
+  mov ah, 0x0e
   int 0x10
 
   jmp .loop
 
 .done:
+  pop bx
   pop ax
   pop si
   ret
 
-main:
-  ; setup data segment
-  ; can't write to ds/es directly
-  mov ax, 0      
-  mov ds, ax
-  mov es, ax
-
-  ; setup stack
-  mov ss, ax
-  ; stack grows downwards from where we loaded in memory
-  mov sp, 0x7C00
-
-  ; print message
-  mov si, msg_hello
-  call puts
-
-  hlt
-
-.halt:
-  jmp .halt
-
-msg_hello: db 'Hello World!', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello World from new kernel!', ENDL, 0
